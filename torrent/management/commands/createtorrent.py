dbb9616db62bfa321652cb77df799a1206e1de73
Aurelien Martin <contact@bitiso.org>
2024-01-28 16:04:56 +0000

    Update createtorrent.py

--- torrent/management/commands/createtorrent.py
+++ torrent/management/commands/createtorrent.py
@@ -9,12 +9,7 @@ import re
 import hashlib
 
 class Command(BaseCommand):
-
-    help = "Create a torrent (metainfo file) from a file or a directory"
-
-    def add_arguments(self, parser):
-        # Optional argument
-        parser.add_argument('-p', '--path', type=str, help='Define path of data to be created as torrent')
+    help = "Create torrents (metainfo files) for files and directories in TORRENT_DATA_TMP"
 
     def create_torrent_and_move(self, file_path):
         # Check if the file is empty
@@ -29,13 +24,16 @@ class Command(BaseCommand):
             print("File: {} already exists. Skip".format(absolute_path_torrent))
             return
 
-        # Hash the file with SHA-256
-        sha256_hash = hashlib.sha256()
+        # Calculate the SHA-256 hash of the file
+        sha256_hash_value = hashlib.sha256()
+        # Calcul du hachage SHA-256 du fichier
+        sha256_hash_value = hashlib.sha256()
         with open(file_path, "rb") as f:
             for byte_block in iter(lambda: f.read(4096), b""):
-                sha256_hash.update(byte_block)
-        sha256 = sha256_hash.hexdigest()
-        print("File hash {}: {}".format(file_path, sha256))
+                sha256_hash_value.update(byte_block)
+        hash_signature = sha256_hash_value.hexdigest()
+
+        print("File hash {}: {}".format(file_path, hash_signature))
 
         # Create the metainfo file (.torrent)
         now = datetime.datetime.now()
@@ -97,7 +95,7 @@ class Command(BaseCommand):
             metainfo_file='torrent/' + t.name + '.torrent',
             file_list=file_list,
             file_nbr=len(t.files),
-            hash_signature=sha256
+            hash_signature=hash_signature
         )
         obj.save()
 
@@ -111,12 +109,86 @@ class Command(BaseCommand):
         print("Moving file {} to: {}".format(file_path, dst_path))
         shutil.move(file_path, dst_path)
 
-    def create_torrent_for_directory_and_move(self, dir_path):
-        print("Creating torrents for files inside directory: {}".format(dir_path))
-        for root, dirs, files in os.walk(dir_path):
-            for file in files:
-                file_path = os.path.join(root, file)
-                self.create_torrent_and_move(file_path)
+    def create_torrent_for_directory_and_move(self, dir_path, hash_signature):
+
+        print("Creating torrent for directory: {}".format(dir_path))
+
+        # Check if the directory is empty
+        if not os.listdir(dir_path):
+            print("Directory: {} is empty. Skip".format(dir_path))
+            return
+
+        # Create a torrent for the entire directory
+        dir_name = os.path.basename(dir_path)
+        torrent_dir_path = os.path.join(settings.TORRENT_DATA_TMP, dir_name + '.torrent')
+
+        # Check if the torrent file already exists
+        if os.path.isfile(torrent_dir_path):
+            print("The torrent file for the directory already exists: {}. Skip".format(torrent_dir_path))
+            return
+
+        t = Torrenttorf(
+            dir_path,
+            trackers=settings.TRACKER_ANNOUNCE,
+            comment='',
+            created_by='Bitiso.org',
+            creation_date=datetime.datetime.now()
+        )
+        t.private = True
+        t.generate()
+
+        # Check if infohash exists in the database
+        if Torrent.objects.filter(info_hash=t.infohash).exists():
+            print("Info hash {} already exists in DB".format(t.infohash))
+            return
+
+        # Path to the torrent file for the directory
+        torrent_file_path = os.path.join(settings.TORRENT_FILES, dir_name + '.torrent')
+
+        # Check if the torrent file already exists
+        if os.path.isfile(torrent_file_path):
+            print(
+                "The torrent file for the directory already exists and will be replaced: {}".format(torrent_file_path))
+            os.remove(torrent_file_path)
+
+        t.write(torrent_file_path)
+        print("Write torrent for directory: {}".format(t.name + '.torrent'))
+
+        # Insert unknown trackers into the database
+        list_of_tracker_id = []
+        for tracker_url in t.trackers:
+            tracker_url_sanitized = re.search('\'(.*)\'', str(tracker_url), re.IGNORECASE).group(1)
+            if not Tracker.objects.filter(url=tracker_url_sanitized).exists():
+                print('Insert new tracker: {}'.format(tracker_url_sanitized))
+                tracker = Tracker(url=tracker_url_sanitized)
+                tracker.save()
+                list_of_tracker_id.append(tracker.id)
+            list_of_tracker_id.append(Tracker.objects.get(url=tracker_url_sanitized).id)
+        print(list_of_tracker_id)
+
+        # Format file list
+        file_list = ''
+        for i in t.files:
+            file_list += '{};{}\n'.format(i.name, i.size)
+
+        # Insert general metadata into the database
+        obj = Torrent(
+            info_hash=t.infohash,
+            name=t.name,
+            size=t.size,
+            pieces=t.pieces,
+            piece_size=t.piece_size,
+            magnet=t.magnet(),
+            torrent_filename=t.name + '.torrent',
+            metainfo_file='torrent/' + t.name + '.torrent',
+            file_list=file_list,
+            file_nbr=len(t.files),
+            hash_signature=hashlib.sha256()
+        )
+        obj.save()
+
+        for tracker_id in list_of_tracker_id:
+            obj.trackers.add(tracker_id)
 
     def handle(self, *args, **kwargs):
         # Get the path from the command line arguments or use the default
@@ -147,4 +219,5 @@ class Command(BaseCommand):
 
             # Check if it's a directory
             if os.path.isdir(item_path):
-                self.create_torrent_for_directory_and_move(item_path)
+                hash_signature = hashlib.sha256()  # Calcul du hachage SHA-256
+                self.create_torrent_for_directory_and_move(item_path, hash_signature)
