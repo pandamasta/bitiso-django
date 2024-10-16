59935c860ab43a249e4b9e7b08e36391cb93fd04
Aurelien Martin <contact@bitiso.org>
2024-10-16 21:37:30 +0000

    Reorganize accounts/templates

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,15 @@
 # accounts/admin.py
+# from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
+# from .models import CustomUser
+
+# # Register the CustomUser model with the Django admin
+# admin.site.register(CustomUser, UserAdmin)
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-# Register the CustomUser model with the Django admin
-admin.site.register(CustomUser, UserAdmin)
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    pass
