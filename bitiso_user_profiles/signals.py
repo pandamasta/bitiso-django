d473f700d41bd026c7c918687e4159907cb0cdb8
Aurelien Martin <contact@bitiso.org>
2024-10-26 18:37:32 +0000

    Fix usercreate profile if missing at creation

--- bitiso_user_profiles/signals.py
+++ bitiso_user_profiles/signals.py
@@ -6,17 +6,39 @@ from .models import BitisoUserProfile
 
 User = get_user_model()
 
-# Automatically create a BitisoUserProfile when a new user is created
+# # Automatically create a BitisoUserProfile when a new user is created
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         BitisoUserProfile.objects.create(user=instance)
+
+# # Ensure the profile is saved whenever the user is saved
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     # Ensure that the profile exists before trying to save it
+#     if hasattr(instance, 'bitisouserprofile'):
+#         instance.bitisouserprofile.save()
+
+
+
+User = get_user_model()
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    """
+    Automatically create a BitisoUserProfile when a new CustomUser is created.
+    """
     if created:
         BitisoUserProfile.objects.create(user=instance)
 
-# Ensure the profile is saved whenever the user is saved
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    # Ensure that the profile exists before trying to save it
-    if hasattr(instance, 'bitisouserprofile'):
+    """
+    Ensure the BitisoUserProfile is saved whenever the CustomUser is saved.
+    If the profile does not exist, create it.
+    """
+    # Check if the profile exists, create it if not
+    if not hasattr(instance, 'bitisouserprofile'):
+        BitisoUserProfile.objects.create(user=instance)
+    else:
         instance.bitisouserprofile.save()
-
-
